de1e264d7b7488310cf8f08fa941565369571727
jyo8141 <xyz@email.com>
2023-01-15 18:39:48 +0400

    add more projects

--- Chapter08/multiQuiz.py
+++ Chapter08/multiQuiz.py
@@ -2,56 +2,38 @@ import random, time
 from inputimeout import inputimeout
 
 
-def prompt():
-        global limit
-        try:
-            answer = inputimeout(prompt="Enter the answer1: ".strip(), timeout=8)
-            limit += 1
-            print(limit)
-            if limit == 3:
-                print("No. of tries is only 3!!!")
-                return False
-            return answer
-        except Exception:
-            print('Your time is over!')
-            return None
-
-def check(ans):
-        global correctAnswers
-        if ans == str(n1 * n2):
-            print('Correct!!')
-            time.sleep(1)
-            correctAnswers += 1
-        else:
-            print('Incorrect!!Try again')
-            answer = prompt()
-            check(answer)
-
-
 numberOfQuestions = 10
 correctAnswers = 0
 for questionNumber in range(numberOfQuestions):
     n1 = random.randint(0, 9)
     n2 = random.randint(0, 9)
 
+    limit = 1
     print(f'{questionNumber+1}: {n1} x {n2} = ')
-    
+    corr_ans = str(n1*n2)
+
     try:
         ans = inputimeout(prompt="Enter the answer: ".strip(), timeout=8)
-        limit = 1
     except Exception:
         print('Your time is over!')
         continue
 
-    while not ans.isdigit():
-        print("Enter a number")
-        ans = prompt()
-        print('hi')
-        print('ans: ', ans)
-        if ans ==  False:
+    while (not ans.isdigit()) or ans != corr_ans:
+        print("Enter a the correct answer")
+        try:
+            ans = inputimeout(prompt="Enter the answer1: ".strip(), timeout=8)
+            if ans == corr_ans:
+                    break
+            limit += 1
+            if limit == 3:
+                print("No. of tries is only 3!!!")
+                break
+        except Exception:
+            print('Your time is over!')
             break
-    if ans == False:
-        continue
-    check(ans)
-    
+    if ans == corr_ans:
+        print('Correct!!\n')
+        time.sleep(1)
+        correctAnswers += 1   
+        
 print(f'Score: {correctAnswers} / {numberOfQuestions}')
